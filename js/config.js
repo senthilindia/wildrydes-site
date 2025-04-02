ec1924c16eacc5dd422b8d0d23aeaf4da80f4c0c
senthilindia <33576382+senthilindia@users.noreply.github.com>
2025-04-02 10:53:13 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_4WGL2KtIR', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '32vraru8ddrvl62nnjc43nv6k1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: 'bc1bl0s18o0qggjfucgg4u02ae0khvnh3igsnq25l0kgr7bc6m7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
