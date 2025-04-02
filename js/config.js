4a3e82fbdea322079bb76548272702c90b995a47
senthilindia <33576382+senthilindia@users.noreply.github.com>
2025-04-02 10:47:55 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_4WGL2KtIR', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '32vraru8ddrvl62nnjc43nv6k1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
